c93c111cd1ee787a9586c0a8af3e2e300422a4f9
Jake Quinter <hello@jakequinter.io>
2022-07-06 19:07:55 -0400

    init

---
+++ DesignCodeiOS15/ContentView.swift
@@ -0,0 +1,58 @@
+//
+//  ContentView.swift
+//  DesignCodeiOS15
+//
+//  Created by Jake Quinter on 7/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            Spacer()
+            
+            Image("Logo 2")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 26, height: 26)
+                .cornerRadius(20)
+            
+            Text("SwiftUI for iOS 15")
+                .font(.largeTitle.bold())
+                .foregroundStyle(.linearGradient(colors: [.primary, .primary.opacity(0.5)], startPoint: .topLeading, endPoint: .bottomTrailing))
+            
+            Text("20 sections - 3 hours".uppercased())
+                .font(.footnote.weight(.semibold))
+                .foregroundStyle(.secondary)
+            
+            Text("Build an iOS app for iOS 15 with custom loayous, animations and ...")
+                .font(.footnote)
+                .foregroundColor(.secondary)
+                .multilineTextAlignment(.leading)
+                .lineLimit(2)
+                .frame(maxWidth: .infinity, alignment: .leading)
+        }
+        .padding(.all, 20)
+        .padding(.vertical, 20)
+        .frame(height: 350)
+        .background(.ultraThinMaterial)
+        .cornerRadius(30)
+        .shadow(color: Color("Shadow").opacity(0.3), radius: 10, x: 0, y: 10)
+        .padding(.horizontal, 20)
+        .background(Image("Blob 1").offset(x: 250, y: -100))
+        .overlay(
+            Image("Illustration 5")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(height: 230)
+                .offset(x: 32, y: -80)
+        )
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DesignCodeiOS15/DesignCodeiOS15App.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeiOS15App.swift
+//  DesignCodeiOS15
+//
+//  Created by Jake Quinter on 7/6/22.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeiOS15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
