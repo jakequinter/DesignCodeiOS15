c78ea44eec9b34175f75667c3d6cc1d50b6d2d88
Jake Quinter <hello@jakequinter.io>
2022-07-07 18:33:54 -0400

    account view

--- DesignCodeiOS15/ContentView.swift
+++ DesignCodeiOS15/ContentView.swift
@@ -16,7 +16,10 @@ struct ContentView: View {
                 .resizable(resizingMode: .stretch)
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 26, height: 26)
-                .cornerRadius(20)
+                .cornerRadius(10)
+                .padding(9)
+                .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 16, style: .continuous))
+                .strokeStyle(cornerRadius: 16)
             
             Text("SwiftUI for iOS 15")
                 .font(.largeTitle.bold())
@@ -36,9 +39,9 @@ struct ContentView: View {
         .padding(.all, 20)
         .padding(.vertical, 20)
         .frame(height: 350)
-        .background(.ultraThinMaterial)
-        .cornerRadius(30)
+        .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 30, style: .continuous))
         .shadow(color: Color("Shadow").opacity(0.3), radius: 10, x: 0, y: 10)
+        .strokeStyle()
         .padding(.horizontal, 20)
         .background(Image("Blob 1").offset(x: 250, y: -100))
         .overlay(
