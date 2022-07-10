35a55cc02261dd32aba0a4a7061309ffd77d9f25
Jake Quinter <hello@jakequinter.io>
2022-07-10 17:18:21 -0400

    custom navbar

--- DesignCodeiOS15/Views/HomeView.swift
+++ DesignCodeiOS15/Views/HomeView.swift
@@ -8,48 +8,34 @@
 import SwiftUI
 
 struct HomeView: View {
+    @State private var hasScrolled = false
+    
     var body: some View {
-        VStack(alignment: .leading, spacing: 8) {
-            Spacer()
+        ScrollView {
+            GeometryReader { proxy in
+                Color.clear.preference(key: ScrollPreferenceKey.self, value: proxy.frame(in: .named("scroll")).minY)
+            }
+            .frame(height: 0)
             
-            Image("Logo 2")
-                .resizable(resizingMode: .stretch)
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 26, height: 26)
-                .cornerRadius(10)
-                .padding(9)
-                .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 16, style: .continuous))
-                .strokeStyle(cornerRadius: 16)
+            FeaturedItem()
             
-            Text("SwiftUI for iOS 15")
-                .font(.largeTitle.bold())
-                .foregroundStyle(.linearGradient(colors: [.primary, .primary.opacity(0.5)], startPoint: .topLeading, endPoint: .bottomTrailing))
-            
-            Text("20 sections - 3 hours".uppercased())
-                .font(.footnote.weight(.semibold))
-                .foregroundStyle(.secondary)
-            
-            Text("Build an iOS app for iOS 15 with custom loayous, animations and ...")
-                .font(.footnote)
-                .foregroundColor(.secondary)
-                .multilineTextAlignment(.leading)
-                .lineLimit(2)
-                .frame(maxWidth: .infinity, alignment: .leading)
+            Color.clear.frame(height: 1000)
         }
-        .padding(.all, 20)
-        .padding(.vertical, 20)
-        .frame(height: 350)
-        .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 30, style: .continuous))
-        .shadow(color: Color("Shadow").opacity(0.3), radius: 10, x: 0, y: 10)
-        .strokeStyle()
-        .padding(.horizontal, 20)
-        .background(Image("Blob 1").offset(x: 250, y: -100))
+        .coordinateSpace(name: "scroll")
+        .onPreferenceChange(ScrollPreferenceKey.self, perform: { value in
+            withAnimation(.easeInOut) {
+                if value < 0 {
+                    hasScrolled = true
+                } else {
+                    hasScrolled = false
+                }
+            }
+        })
+        .safeAreaInset(edge: .top, content: {
+            Color.clear.frame(height: 70)
+        })
         .overlay(
-            Image("Illustration 5")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(height: 230)
-                .offset(x: 32, y: -80)
+            NavigationBar(title: "Featured", hasScrolled: $hasScrolled)
         )
     }
 }
